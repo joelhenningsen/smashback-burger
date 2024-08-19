cbe5a571e40588a649b64758b931e36b4bdc2c7e
joelhenningsen <ravajoely@gmail.com>
2024-08-19 14:29:42 -0700

    Initial website setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'smashYellow': '#FBE2B9',
+        'smashRed': '#EA0000',
+      },
+    }
+  },
+  plugins: [],
+}
+
